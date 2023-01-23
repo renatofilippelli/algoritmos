b3dbac2b978cd777ae5ccb7c6932042659c082a4
Renato Filippelli <renatofilippelli@gmail.com>
2023-01-23 00:16:18 -0300

    o(n ao quatrado) para o(n)

---
+++ Main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	entrada := []int{2, 3, 4, 55, 6, 7}
+	esperado := 62
+	fmt.Println(calc(entrada, esperado, true))
+
+	entrada = []int{2, 3, 4, 55, 6, 7}
+	esperado = 12
+	fmt.Println(calc(entrada, esperado, true))
+
+}
+
+func calc(entrada []int, esperado int, b bool) bool {
+
+	//array int para string pura
+	entradaTexto := strings.Trim(strings.Replace(fmt.Sprint(entrada), " ", " ", -1), "[]")
+
+	//percorrer array
+	for a := 0; a < len(entrada); a++ {
+
+		//pega valor do arragity na posição a
+		valor1 := entrada[a]
+
+		//calcula do resultado esperado atual do array posição a
+		resultado := strconv.Itoa(esperado - valor1)
+
+		//string sem o valor da posição a
+		stringarray := entradaTexto[strings.Index(entradaTexto, strconv.Itoa(valor1))+1:]
+
+		//procura na string pura se existe resultado esperado
+		if strings.Contains(stringarray, resultado) {
+			return true
+		}
+	}
+
+	return false
+}
